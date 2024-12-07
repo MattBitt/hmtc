4f138c896f463360de8a447b3e935bc99513a571
Matt Bittinger <mattsbitt@gmail.com>
2024-12-07 17:13:44 -0600

    converting series to domain

--- hmtc/domains/channel.py
+++ hmtc/domains/channel.py
@@ -21,18 +21,15 @@ class Channel:
 
     @classmethod
     def create(cls, data) -> ChannelModel:
-        new_channel = cls.repo.create_item(data=data)
-        return new_channel
+        return cls.repo.create_item(data=data)
 
     @classmethod
     def load(cls, item_id) -> ChannelModel:
-        _item = cls.repo.load_item(item_id=item_id)
-        return _item
+        return cls.repo.load_item(item_id=item_id)
 
     @classmethod
     def update(cls, data) -> ChannelModel:
-        _item = cls.repo.update_item(data=data)
-        return _item
+        return cls.repo.update_item(data=data)
 
     @classmethod
     def get_all(cls) -> List[ChannelModel]:
@@ -49,21 +46,15 @@ class Channel:
         return item.simple_dict()
 
     @staticmethod
-    def get_by_id(item_id) -> ChannelModel:
-        return ChannelModel.get_by_id(item_id)
-
-    @staticmethod
     def last_update_completed() -> str | None:
         channel = (
-            (
-                ChannelModel.select(ChannelModel.last_update_completed).where(
-                    ChannelModel.auto_update == True
-                )
-            )
+            ChannelModel.select(ChannelModel.last_update_completed)
+            .where(ChannelModel.auto_update == True)
             .order_by(ChannelModel.last_update_completed.desc())
             .limit(1)
             .get_or_none()
         )
+
         if channel:
             return str(channel.last_update_completed)
 
