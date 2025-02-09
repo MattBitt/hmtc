0ee69249598fcda09d9a65c669a10286dfcd1ce0
Matt Bittinger <mattsbitt@gmail.com>
2025-02-09 14:26:41 -0600

    deploy: again

--- hmtc/pages/discs/details.py
+++ hmtc/pages/discs/details.py
@@ -27,93 +27,8 @@ def SecondRow(
 ):
     error = solara.use_reactive("")
     success = solara.use_reactive("")
-    unique = solara.use_reactive(video.instance.unique_content)
-
-    def assign_album(title):
-        album = Album.get_by(title=title)
-        if album is None:
-            error.set(f"Album Not Found")
-            return
-        album.add_video(video.instance)
-        success.set(f"{video.instance.title} added to {album.instance.title}")
-        refresh_counter.set(refresh_counter.value + 1)
-
-    def toggle_unique():
-        new_unique = not video.instance.unique_content
-        video.instance.unique_content = new_unique
-        video.instance.save()
-        unique.set(new_unique)
-        refresh_counter.set(refresh_counter.value + 1)
-
-    def delete_video_file():
-        logger.error(f"Deleting video_file for {video.instance.title}")
-        video.delete_file("video")
-        refresh_counter.set(refresh_counter.value + 1)
-
-    with solara.Columns([8, 4]):
-        with solara.Card("Album"):
-            with solara.ColumnsResponsive():
-                solara.Button(
-                    "Energy Exchange Tour",
-                    on_click=lambda: assign_album("Energy Exchange Tour"),
-                    classes=["button"],
-                )
-                solara.Button(
-                    "Happy Hour",
-                    on_click=lambda: assign_album("Happy Hour"),
-                    classes=["button"],
-                )
-                solara.Button(
-                    "Interviews",
-                    on_click=lambda: assign_album("Interviews"),
-                    classes=["button"],
-                )
-                solara.Button(
-                    "Livestream Highlights",
-                    on_click=lambda: assign_album("Livestream Highlights"),
-                    classes=["button"],
-                )
-                solara.Button(
-                    "No Where Else to Put These",
-                    on_click=lambda: assign_album("No Where Else to Put These"),
-                    classes=["button"],
-                )
-                solara.Button(
-                    "Odyssey Tour",
-                    on_click=lambda: assign_album("Odyssey Tour"),
-                    classes=["button"],
-                )
-                solara.Button(
-                    "On the Street",
-                    on_click=lambda: assign_album("On the Street"),
-                    classes=["button"],
-                )
-            with solara.Row():
-                if error.value != "":
-                    solara.Error(error.value)
-                    solara.Button(
-                        f"Clear", on_click=lambda: error.set(""), classes=["button"]
-                    )
-                elif success.value != "":
-                    solara.Success(success.value)
-                    solara.Button(
-                        f"Clear", on_click=lambda: success.set(""), classes=["button"]
-                    )
-        with solara.Card("Other"):
-
-            with solara.ColumnsResponsive():
-                solara.Button(
-                    f"Unique: {str(unique.value)}",
-                    on_click=toggle_unique,
-                    classes=["button"],
-                )
-                solara.Button(
-                    f"Delete Video File",
-                    icon_name="mdi-delete",
-                    on_click=delete_video_file,
-                    classes=["button mywarning"],
-                    disabled=video.video_file() is None,
-                )
+    with solara.Card():
+        solara.Text(f"Video: {video.instance.title}")
 
 
 @solara.component
@@ -125,6 +40,7 @@ def MainRow(disc: Disc):
     with solara.Card():
         with solara.Row(justify="center"):
             solara.Text(f"{disc.instance.title}")
+            solara.Text(f"{disc.instance.album.title}")
 
 
 @solara.component
@@ -148,12 +64,11 @@ def DiscEditor(disc: Disc):
     if current_page.value > num_pages:
         current_page.set(num_pages)
 
-    video = Video(_query.get())
-
     with solara.Row(justify="center"):
         MainRow(disc)
-    with solara.Row(justify="center"):
-        SecondRow(video)
+
+    for video in _query:
+        SecondRow(Video(video))
     with solara.Row(justify="center"):
         PaginationControls(
             current_page=current_page, num_pages=num_pages, num_items=num_items
